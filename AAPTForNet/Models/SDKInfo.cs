d33c5630f2bc14a6244f5bc9407057ff918b77c1
Phương Trần <36994503+canheo136@users.noreply.github.com>
2020-07-16 18:46:32 +0700

    Fix #1

--- AAPTForNet/Models/SDKInfo.cs
+++ AAPTForNet/Models/SDKInfo.cs
@@ -5,49 +5,63 @@
         private static readonly string[] AndroidCodeName = {
             "Unknown",
 
-            "Cupcake", // 3
-            "Donut", // 4
-            "Eclair", "Eclair", "Eclair", // 5, 6, 7
-            "Froyo", // 8
-            "Gingerbread", "Gingerbread", // 9, 10
-            "Honeycomb", "Honeycomb", "Honeycomb", // 11, 12, 13
-            "Ice Cream Sandwich", "Ice Cream Sandwich", // 14, 15
-            "Jelly Bean", "Jelly Bean", "Jelly Bean",   // 16, 17, 18
-            "KitKat",   // 19
-            "Unknown",  // 20
-            "Lollipop", "Lollipop", //  21, 22
-            "Marshmallow",  // 23
-            "Nougat", "Nougat", // 24, 25
-            "Oreo", "Oreo", // 26, 27
-            "Pie",  // 28
+            "Cupcake",  // API level 3
+            "Donut",
+            "Eclair",
+            "Eclair",
+            "Eclair",
+            "Froyo",
+            "Gingerbread",
+            "Gingerbread",
+            "Honeycomb",
+            "Honeycomb",
+            "Honeycomb",
+            "Ice Cream Sandwich",
+            "Ice Cream Sandwich",
+            "Jelly Bean",
+            "Jelly Bean",
+            "Jelly Bean",
+            "KitKat",
+            "Unknown",  // API level 20
+            "Lollipop",
+            "Lollipop",
+            "Marshmallow",
+            "Nougat",
+            "Nougat",
+            "Oreo",
+            "Oreo",
+            "Pie",  // API level 28
         };
 
         private static readonly string[] AndroidVersionCode = {
             "Unknown",
 
-            "1.5",  // 3
-            "1.6",  // 4
-            "2.0", "2.0",  // 5, 6
-            "2.1",  // 7
-            "2.2",  // 8
-            "2.3", "2.3",  // 9, 10
-            "3.0",  // 11
-            "3.1",  // 12
-            "3.2",  // 13
-            "4.0", "4.0",   // 14, 15
-            "4.1",  // 16
-            "4.2",  // 17
-            "4.3",  // 18
-            "4.4",  // 19
-            "Unknown",  // 20
-            "5.0",  // 21
-            "5.1",  // 22
-            "6.0",  // 23
-            "7.0",  // 24
-            "7.1",  // 25
-            "8.0",  // 26
-            "8.1",  // 27
-            "9",    // 28
+            "1.5",  // API level 3
+            "1.6",
+            "2.0",
+            "2.0",
+            "2.1",
+            "2.2",
+            "2.3",
+            "2.3",
+            "3.0",
+            "3.1",
+            "3.2",
+            "4.0",
+            "4.0",
+            "4.1",
+            "4.2",
+            "4.3",
+            "4.4",
+            "Unknown",  // API level 20
+            "5.0",
+            "5.1",
+            "6.0",
+            "7.0",
+            "7.1",
+            "8.0",
+            "8.1",
+            "9",    // API level 28
         };
 
         public string APILever { get; }
@@ -61,7 +75,7 @@
         }
 
         public static SDKInfo GetInfo(int sdkVer) {
-            int index = (sdkVer < 14 || sdkVer > 28) ? 0 : sdkVer - 13;
+            int index = (sdkVer < 3 || sdkVer > 28) ? 0 : sdkVer - 2;
 
             return new SDKInfo(sdkVer.ToString(), AndroidVersionCode[index], AndroidCodeName[index]);
         }
