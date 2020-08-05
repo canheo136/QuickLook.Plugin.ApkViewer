d66b0775d7033d386920ff2f966614f4f3932295
Cá Nheo <canheo136@gmail.com>
2020-08-05 23:22:23 +0700

    #2: Modern UI font #3: Add target SDK

--- ViewerPane.xaml.cs
+++ ViewerPane.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -26,41 +27,62 @@ namespace QuickLook.Plugin.ApkViewer {
             InitializeComponent();
 
             btnSwTheme.Source = new BitmapImage(new Uri(AAPTool.AppPath + @"\images\switch_theme.png"));
-            btnSwTheme.MouseLeftButtonDown += switchTheme;
+            btnSwTheme.MouseLeftButtonDown += (sender, e) => {
+                bool isDark = _quickLook.Theme == Themes.Dark;
+
+                // Make some effect with switch button.
+                btnSwTheme.RenderTransform = new RotateTransform(
+                    isDark ? 180 : 0,
+                    btnSwTheme.Width / 2,
+                    btnSwTheme.Height / 2
+                );
+                this.Resources["TextForeground"] = isDark ? Brushes.Black : Brushes.White;
+                _quickLook.Theme = isDark ? Themes.Light : Themes.Dark;
+            };
         }
 
         public ViewerPane(ContextObject ql) : this() {
             _quickLook = ql;
-        }
-
-        private void viewImage(object sender, MouseButtonEventArgs e) {
-            e.Handled = true;
-            System.Diagnostics.Process.Start("explorer.exe", ApkInfo.IconPath);
-        }
-
-        private void switchTheme(object sender, MouseButtonEventArgs e) {
-            e.Handled = true;
-            // Make some effect with switch button.
-            btnSwTheme.RenderTransform = new RotateTransform(
-                _quickLook.Theme == Themes.Dark ? 180 : 0,
-                btnSwTheme.Width / 2,
-                btnSwTheme.Height / 2
-            );
-
-            _quickLook.Theme = (_quickLook.Theme == Themes.Dark) ? Themes.Light : Themes.Dark;
+            
+            // Highlight label on QuickLook default theme
+            this.Resources["TextForeground"] = ql.Theme == Themes.Dark ?
+                Brushes.White : Brushes.Black;
         }
 
         private void initGUI() {
             labelAppName.Content = ApkInfo.AppName;
             labelPckName.Content = ApkInfo.PackageName;
             labelVer.Content = ApkInfo.Version;
-            labelSDK.Content = ApkInfo.MinSDK;
-            labelSize.Content = ApkInfo.PackageSize.ToPrettySize(2);
+            labelMinSDK.Content = ApkInfo.MinSDK;
+            labelTargetSDK.Content = ApkInfo.TargetSDK;
+            labelSupportScr.Content = string.Join(", ", ApkInfo.SupportScreens);
+            labelPckSize.Content = ApkInfo.PackageSize.ToPrettySize(2);
+
+            if (ApkInfo.Permissions.Count != 0) {
+                foreach (string per in ApkInfo.Permissions) {
+                    permissionStack.Children.Add(new Label() {
+                        // Disable access key (mnemonics)
+                        // https://docs.microsoft.com/en-us/dotnet/api/system.windows.controls.label#remarks
+                        Content = per.Replace("_", "__"),
+                        Style = (Style)this.Resources["HoverableLabel"]
+                    });
+                }
+            }
+            else {
+                panelPermission.Content = new Label() {
+                    Content = "This package does not require any permissions",
+                    VerticalAlignment = VerticalAlignment.Center,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    Style = (Style)this.Resources["LabelBaseStyle"]
+                };
+            }
 
             string uri = ApkInfo.IconPath;
 
-            if(ApkInfo.IconPath != string.Empty) {
-                image.MouseLeftButtonDown += viewImage;
+            if (ApkInfo.IconPath != string.Empty) {
+                image.MouseLeftButtonDown += (sender, e) => {
+                    System.Diagnostics.Process.Start("explorer.exe", ApkInfo.IconPath);
+                };
             }
             else {
                 image.ToolTip = "Default icon";
