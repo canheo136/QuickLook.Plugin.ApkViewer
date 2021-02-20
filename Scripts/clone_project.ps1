f05633d67fbfb85faadc4ebb4d92df0cba1643dd
canheo136 <36994503+canheo136@users.noreply.github.com>
2021-02-20 22:00:32 +0700

    Add clone script

---
+++ Scripts/clone_project.ps1
@@ -0,0 +1,5 @@
+git clone https://github.com/canheo136/QuickLook.Plugin.ApkViewer.git
+
+cd QuickLook.Plugin.ApkViewer
+del QuickLook.Common/*.md
+git clone https://github.com/QL-Win/QuickLook.Common
