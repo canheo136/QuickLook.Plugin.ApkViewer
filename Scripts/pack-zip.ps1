fa00d707b9d8e9ed2a5fcd83b52db1fd0bba3f52
canheo136 <canheo136@gmail.com>
2022-01-23 15:32:06 +0700

    Add post-build script

--- Scripts/pack-zip.ps1
+++ Scripts/pack-zip.ps1
@@ -1,5 +1,9 @@
 Remove-Item ..\QuickLook.Plugin.ApkViewer.qlplugin -ErrorAction SilentlyContinue
 
-$files = Get-ChildItem -Path ..\bin\release\ -Exclude *.pdb,*.xml
-Compress-Archive $files ..\QuickLook.Plugin.ApkViewer.zip
-Move-Item ..\QuickLook.Plugin.ApkViewer.zip ..\QuickLook.Plugin.ApkViewer.qlplugin
+$files = Get-ChildItem -Path ..\release\ -Exclude *.pdb,*.xml
+$outputPlugin = '..\..\QuickLook.Plugin.ApkViewer.qlplugin'
+
+Compress-Archive $files ..\QuickLook.Plugin.ApkViewer.zip -Force
+Move-Item ..\QuickLook.Plugin.ApkViewer.zip $outputPlugin -Force
+
+[string]::Concat("Packed plugin -> ", [System.IO.Path]::GetFullPath($outputPlugin))
