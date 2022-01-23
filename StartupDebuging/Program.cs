df6111c71e2eabdb4a19462e23898bcf6cdc60a1
canheo136 <canheo136@gmail.com>
2022-01-23 15:23:53 +0700

    Add debug project

---
+++ StartupDebuging/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using AAPTForNet;
+
+namespace StartupDebuging {
+    class Program {
+
+        static readonly string[] APK_FILES = Directory.GetFiles(Path.Combine(Directory.GetCurrentDirectory(), "apks"), "*.apk");
+
+        public static void Main(string[] args) {
+
+            if (APK_FILES == null || APK_FILES.Length == 0)
+                throw new Exception("There are no apk files to run AAPT");
+
+            var apks = APK_FILES.Take(20).Select(f => {
+
+                try {
+                    var apk = AAPTool.Decompile(f);
+                    return apk;
+                }
+                catch (Exception ex) {
+                    Console.WriteLine(ex);
+                    Console.WriteLine("===========================================");
+                    return null;
+                }
+
+            }).ToList();
+
+#pragma warning disable CS0219
+            var breakPoint = "Break me";
+        }
+    }
+}
